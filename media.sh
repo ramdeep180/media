da57a71f7111ddd1da78128745a2ba6c68d156c8
root <root@ip-172-31-91-199.ec2.internal>
2020-04-18 06:12:29 +0000

    chaned all echo values

--- media.sh
+++ media.sh
@@ -6,6 +6,6 @@ IID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 IIT=$(curl http://169.254.169.254/latest/meta-data/instance-type)
 echo "The public host name is $PH"
 echo "The public sg name is $PSG"
-echo "$PIP"
-echo "$IID"
-echo "$IIT"
+echo "The pip address is $PIP"
+echo "The Instance ID is $IID"
+echo "The Instance type is $IIT"
