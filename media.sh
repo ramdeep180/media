acee68a52af4397f823401437d65681cd082d556
root <root@ip-172-31-91-199.ec2.internal>
2020-04-18 05:58:49 +0000

    updated ph name and sg name

--- media.sh
+++ media.sh
@@ -4,8 +4,8 @@ PSG=$(curl http://169.254.169.254/latest/meta-data/security-groups)
 PIP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 IID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 IIT=$(curl http://169.254.169.254/latest/meta-data/instance-type)
-echo "$PH"
-echo "$PSG"
+echo "The public host name is $PH"
+echo "The public sg name is $PSG"
 echo "$PIP"
 echo "$IID"
 echo "$IIT"
