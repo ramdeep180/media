9e57ee55e4e10706cdfe7fe47304e076eab56a52
ramdeep <ramdeep180@gmail.com>
2020-04-18 12:08:50 +0530

    all commits added

--- media.sh
+++ media.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "-----------running meta-data now----------"
 PH=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 PSG=$(curl http://169.254.169.254/latest/meta-data/security-groups)
 PIP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 IID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 IIT=$(curl http://169.254.169.254/latest/meta-data/instance-type)
+echo "complter..............!!!!!!!!!"
 echo "The public host name is $PH"
 echo "The public sg name is $PSG"
 echo "The pip address is $PIP"
